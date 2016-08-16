aee9c640f50fc24b1e93d3865537f8e1426602d5
Sven A. Schmidt <sas@finestructure.co>
2016-08-16 12:48:25 +0200

    Updated tests

--- Tests/Console/TerminalTests.swift
+++ Tests/Console/TerminalTests.swift
@@ -8,6 +8,7 @@
 
 import XCTest
 import Foundation
+import POSIX
 
 @testable import Console
 
@@ -17,6 +18,7 @@ class TerminalTests: XCTestCase {
     static let allTests = [
         ("testSubexecute", testSubexecute),
         ("testSize", testSize),
+        ("testPopen", testPopen),
         ]
 
     func testSubexecute() {
@@ -38,14 +40,26 @@ class TerminalTests: XCTestCase {
 
     func testSize() {
         let t = Terminal(arguments: [])
-        XCTAssertEqual(t.size.width, 80)
-        XCTAssertEqual(t.size.height, 24)
+        #if Xcode
+            XCTAssertEqual(t.size.width, 0)
+            XCTAssertEqual(t.size.height, 0)
+        #else
+            XCTAssertEqual(t.size.width, 80)
+            XCTAssertEqual(t.size.height, 24)
+        #endif
     }
 
     func testPopen() {
-        if let res = try? popen(["ls", "-l"]) {
-            print("res: \(res)")
-            XCTAssertEqual(res, "foo")
+        if let res = try? popen(["ls", "-ld", "/tmp"]) {
+            // FIXME: remove after Grand Renaming lands on Linux
+            #if os(OSX)
+                // lrwxr-xr-x@ 1 root  wheel  11 10 Sep  2015 /tmp -> private/tmp
+                let parts = res.components(separatedBy: CharacterSet.whitespaces)
+            #else
+                // drwxrwxrwt 3 root root 4096 Aug  4 11:11 /tmp
+                let parts = res.components(separatedBy: NSCharacterSet.whitespaces())
+            #endif
+            XCTAssertEqual(parts[2], "root")
         } else {
             XCTFail()
         }
